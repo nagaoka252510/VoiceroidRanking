ecc89e9f940f563daec2475a27c8a2ff4ea3fa0e
ih6109 <ih6109@gmail.com>
2018-10-23 20:44:25 +0900

    X-Frame-Options設定を削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-  after_action :allow_iframe
-
-  private
-  def allow_iframe
-    # X-Frame-Oprions設定
-    response.headers["X-Frame-Options"] = "ALLOW-FROM http://voiran.ml"
-  end
 end
