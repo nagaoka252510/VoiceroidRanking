5a1d37b2ba640a3acfded19d1d030dc91a0e3510
ih6109 <ih6109@gmail.com>
2018-10-25 21:23:08 +0900

    redirectを設定

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -4,10 +4,11 @@ class RankingController < ApplicationController
   end
 
   def ranking
-    if params[:ym]
+    if params[:ym] && params[:ym].length == 6
       @videos = Video.where(YEAR_MONTH: params[:ym]).order(VIEW_CNT: :desc).first(100)
+      #@year_month = @videos[0].YEAR_MONTH
     else
-      render "home"
+      redirect_to("/")
     end
   end
 end
