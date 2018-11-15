14a3a7b69364807842f6721b39419a1e51b46aaa
ih6109 <ih6109@gmail.com>
2018-11-15 23:01:00 +0900

    パラメタチェックと前月と翌日への移動リンクを追加

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -4,9 +4,22 @@ class RankingController < ApplicationController
   end
 
   def ranking
-    if params[:ym] && params[:ym].length == 6
+    year_monthes = Video.select(:YEAR_MONTH).distinct.order(:YEAR_MONTH).pluck(:YEAR_MONTH)
+    if params[:ym] && year_monthes.include?(params[:ym])
+      @year_month = params[:ym]
       @videos = Video.where(YEAR_MONTH: params[:ym]).order(VIEW_CNT: :desc).first(100)
       #@year_month = @videos[0].YEAR_MONTH
+      ym_idx = year_monthes.index(@year_month)
+      # 前月の取得
+      @pre_month = nil
+      if ym_idx != 0
+        @pre_month = year_monthes[ym_idx.to_i - 1]
+      end
+      # 翌月の取得
+      @next_month = nil
+      if ym_idx != year_monthes.length - 1
+        @next_month = year_monthes[ym_idx.to_i + 1]
+      end
     else
       redirect_to("/")
     end
